118d494aa88abdf10a4596fc358858f432fbfad6
Aryan Yash <aryanyashishere@gmail.com>
2024-10-31 00:53:51 +0530

    live loading - sanity db - sanity studio - searchStartup - startuptype added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,72 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+// import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch , SanityLive} from "@/sanity/lib/live";
+
+
+export default async function Home({searchParams}: {
+  searchParams : Promise<{query?: string}>
+})
+
+{
+
+  const query = (await searchParams).query;
+  const params = {search : query || null };
+  
+  // const posts = await client.fetch(STARTUPS_QUERY);
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
+  
+  // console.log(JSON.stringify(posts));
+
+//   const posts = [{
+//     _createdAt : new Date(),
+//     views: 55,
+//     author: {_id : 1, name: "Aryan"},
+//     _id : 1,
+//     description: "this is a description",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr3oWv5CB12097HzwwA-V6mmPdd8-ny_v5w&s",
+//     category : "Robots",
+//     title: "Stark Verse" 
+//   }, 
+// ];
+  return (
+
+
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your StartUp, <br />
+          Connect With Entrepreneurs 
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get noticed in Vitual Competitions
+          
+                  </p>
+
+        <SearchForm query={query}/>
+
+      </section>
+
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ?  `Search Results for "${query}"` : 'All Startups'}
+      </p>
+
+      <ul className="mt-7 card_grid">
+        {posts?.length>0 ? (
+          posts.map((post: StartupTypeCard)=>(
+            <StartupCard key={post?._id} post= {post} />
+        ))
+      ): (
+        <p className="no-results" >No Startups Found</p>
+      )}
+      </ul>
+
+    </section>
+    <SanityLive/>
+    </>
+  );
+}
---
+++ sanity/lib/queries.ts
@@ -0,0 +1,19 @@
+import { defineQuery } from "next-sanity";
+
+export const STARTUPS_QUERY = 
+defineQuery(    `
+    
+    *[_type == "startup" && defined(slug.current) && !defined($search) || title match $search ||category match $search || author->name match $search] | order(_createdAt desc)
+{
+    _id,
+    title,
+      author-> {
+        _id, name , slug, image, bio
+      },
+    slug , 
+    _createdAt, 
+    views, 
+    description, 
+    category, 
+    image
+}`)
