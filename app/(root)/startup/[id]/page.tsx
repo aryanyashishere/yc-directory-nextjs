e4e3f0944006415039162745421199e18f56548d
Aryan Yash <aryanyashishere@gmail.com>
2024-10-31 14:53:10 +0530

    startup page added with css and adding markdown-it packages

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -4,21 +4,85 @@
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
+import { formatDate } from '@/lib/utils';
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import markdownit from "markdown-it";
+
+const md = markdownit();
+
 
 export const experimental_ppr = true;
 
-const page = async ({params}: {params: Promise<{id: string}>}) => {
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, {id});
-  
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
-  if(!post) return notFound();
+
+  if (!post) return notFound();
+  const parsedContent = md.render(post?.pitch || "")
 
   return (
-<h1 className='text-3xl'>
-{post.title}
-</h1>  )
+    <>
+
+      <section className='pink_container !min-h-[230px]'>
+        <p className='tag'>{formatDate(post?._createdAt)}</p>
+        <h1 className='heading'>{post.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post.description}</p>
+      </section>
+
+      <section className='section_container'>
+
+        <img src="{post.image}" alt="thumbnail" className='w-full h-auto rounded-xl' />
+
+
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link href={`/user/${post.autho?._id}`} className='flex gap-2 items-center mb-3'>
+
+              <Image
+                src={post.author.image}
+                alt="avatar"
+                width={64}
+                height={64}
+                className='rounded-full drop-shado-lg'
+              />
+
+              <div>
+                <p className="text-20-medium">
+                  {post.author.name}
+                </p>
+                <p className='text-16-medium !text-black-300'>
+                  @{post.author.username}
+                </p>
+              </div>
+            </Link>
+
+            <p className="categor-tage">{post.category}</p>
+          </div>
+          <h3 className='text-30-bold'>
+            Pitch Details
+            {/* we have to install the markdown-it npm package */}
+            {/* after it run it  : npm i --save-dev @types/markdown-it */}
+            </h3>
+            {parsedContent ? (
+              <article className='prose max-w-4xl font-work-sans break-all'
+              dangerouslySetInnerHTML={{__html : parsedContent}}/>
+            ):(
+              <p className='no-result'>
+                No details provided.  
+              </p>
+            )}
+
+          
+        </div>
+
+        <hr className='divider'/> 
+      </section>
+    </>
+
+  )
 }
 
 export default page
